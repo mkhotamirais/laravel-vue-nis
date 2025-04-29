324b48429f672e6a381d98f4f5d208e10088eef9
mkhotamirais <mkhotamirais@gmail.com>
2025-04-29 17:04:03 +0700

    masalah logo dynamic

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,10 @@
 import "./bootstrap";
 import "../css/app.css";
 
-import { createApp, h, watch } from "vue";
-import { createInertiaApp, Head, Link, usePage } from "@inertiajs/vue3";
+import { createApp, h } from "vue";
+import { createInertiaApp, Head, Link } from "@inertiajs/vue3";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy";
 import MainLayout from "./Layouts/MainLayout.vue";
-import { setFaviconByPath } from "./favicon";
 import { register } from "swiper/element/bundle";
 
 createInertiaApp({
@@ -16,34 +15,12 @@ createInertiaApp({
         return page;
     },
     setup({ el, App, props, plugin }) {
-        // createApp({ render: () => h(App, props) })
-        //     .use(plugin)
-        //     .use(ZiggyVue)
-        //     .component("Head", Head)
-        //     .component("Link", Link)
-        //     .mount(el);
-        const app = createApp({ render: () => h(App, props) });
-
-        app.use(plugin)
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
             .use(ZiggyVue)
             .component("Head", Head)
-            .component("Link", Link);
-
-        // 🧠 Tambahkan watcher di sini
-        app.mixin({
-            mounted() {
-                const page = usePage();
-                watch(
-                    () => page.url,
-                    (newUrl) => {
-                        setFaviconByPath(newUrl);
-                    },
-                    { immediate: true }
-                );
-            },
-        });
-
-        app.mount(el);
+            .component("Link", Link)
+            .mount(el);
     },
     progress: {
         // delay: 250,
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="storage/images/logos/logo-yayasan-nurul-iman-sindangkerta-favicon.png"
+        type="image/x-icon">
 
     {{-- Fontawesome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
