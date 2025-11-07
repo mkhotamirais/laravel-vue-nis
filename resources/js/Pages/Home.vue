6646f3788a0aa0299b78e4b0e5f3579c4f5b0179
mkhotamirais <mkhotamirais@gmail.com>
2025-11-07 16:14:30 +0700

    sekali lagi

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -17,19 +17,22 @@ defineProps({
 </script>
 
 <template>
-  <Head>
-    <title>Pendidikan Lengkap: Ponpes, RA, MTs, MA</title>
-    <meta
-      head-key="description"
-      name="description"
-      content="Sekolah Nurul Iman menawarkan pendidikan terpadu dari Ponpes, RA, MTs, hingga MA dengan kurikulum modern dan nilai-nilai islami."
-    />
-  </Head>
-  <HeroSection :heroImages="heroImages" />
-  <ProfileSection />
-  <InformationSection :infos="infos" />
-  <QuoteSection :quotes="quotes" />
-  <FasilitasSection :facilities="facilities" />
-  <DaftarSection />
-  <AgendaSection :agendas="agendas" />
+  <div>
+    <Head>
+      <title>Pendidikan Lengkap: Ponpes, RA, MTs, MA</title>
+      <meta
+        head-key="description"
+        name="description"
+        content="Sekolah Nurul Iman menawarkan pendidikan terpadu dari Ponpes, RA, MTs, hingga MA dengan kurikulum modern dan nilai-nilai islami."
+      />
+    </Head>
+    <p>halo</p>
+    <HeroSection :heroImages="heroImages" />
+    <ProfileSection />
+    <InformationSection :infos="infos" />
+    <QuoteSection :quotes="quotes" />
+    <FasilitasSection :facilities="facilities" />
+    <DaftarSection />
+    <AgendaSection :agendas="agendas" />
+  </div>
 </template>
