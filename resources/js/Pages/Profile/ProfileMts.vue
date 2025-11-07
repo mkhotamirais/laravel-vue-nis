1c0742e0819e47ed14924f4a4545e1f6686b378e
mkhotamirais <mkhotamirais@gmail.com>
2025-11-07 15:16:25 +0700

    perbaikan sedikit

--- resources/js/Pages/Profile/ProfileMts.vue
+++ resources/js/Pages/Profile/ProfileMts.vue
@@ -91,7 +91,7 @@ import cl from "@/content_long.json";
       <!-- Sejarah -->
       <div class="mb-8 max-w-2xl">
         <div class="mb-4">
-          <h2 class="h2">Sejarah RA Nurul Iman</h2>
+          <h2 class="h2">Sejarah MTs Nurul Iman</h2>
         </div>
 
         <article class="leading-relaxed space-y-2">
